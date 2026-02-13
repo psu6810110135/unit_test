74e87cbb5eb2662e42bfa5093c311fd704b0b4d4
psu6810110135 <404tkth@gmail.com>
2026-02-13 15:55:33 +0700

    add test case

---
+++ tests/test_number_utils.py
@@ -0,0 +1,13 @@
+
+from _6810110135_.number_utils import is_prime_list
+import unittest
+
+class PrimeListTest(unittest.TestCase):
+    def test_give_1_2_3_is_prime(self):
+        numbers = [1, 2, 3]
+        is_prime = is_prime_list(numbers)
+        self.assertTrue(is_prime)
+    def test_give_4_5_6_is_not_prime(self):
+        numbers = [4, 5, 6]
+        is_prime = is_prime_list(numbers)
+        self.assertFalse(is_prime)
